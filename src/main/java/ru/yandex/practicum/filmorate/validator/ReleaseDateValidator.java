1ccf5630bc5eb39aac72f522d9c8e9962d10f3d3
Kettle-Drummer <asces@yandex.ru>
2023-12-08 15:00:16 +0600

    style fixed

--- src/main/java/ru/yandex/practicum/filmorate/validator/ReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/ReleaseDateValidator.java
@@ -6,10 +6,12 @@ import java.time.LocalDate;
 
 public class ReleaseDateValidator implements ConstraintValidator<ValidReleaseDate, LocalDate> {
     private LocalDate minimumDate;
+
     @Override
     public void initialize(ValidReleaseDate constraintAnnotation) {
         minimumDate = LocalDate.parse(constraintAnnotation.value());
     }
+
     @Override
     public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
         return value == null || !value.isBefore(minimumDate);
