b7ff12331b03c56779d3bad5819e3791d92f0c9c
Kettle-Drummer <asces@yandex.ru>
2024-01-19 21:32:03 +0600

    style

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -12,6 +12,7 @@ import java.util.*;
 public class InMemoryUserStorage implements UserStorage {
     private final Map<Long, User> users = new HashMap<>();
     private long id;
+
     private long createId() {
         return ++id;
     }
